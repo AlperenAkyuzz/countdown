299f16a75a8b29201a81158a6e7d984ca2430c32
alperen <alperen@endigitals.com>
2021-11-22 21:44:58 +0300

    ddddddd

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
 <template>
   <div>
-    hello from world
+    <div class="square"></div>
+    <div class="circle"></div>
+    <div class="triangle-up"></div>
   </div>
 </template>
 
+<style>
+.square {
+  height: 50px;
+  width: 50px;
+  background-color: #555;
+}
+.circle {
+  height: 50px;
+  width: 50px;
+  background-color: #555;
+  border-radius: 50%;
+}
+.triangle-up {
+  width: 0;
+  height: 0;
+  border-left: 25px solid transparent;
+  border-right: 25px solid transparent;
+  border-bottom: 50px solid #555;
+}
+</style>
 <script>
 export default {}
 </script>
+
